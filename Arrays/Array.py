38b50edd41a523504c4621ce087b634d870d32e7
kunalarya873 <kunalarya873@gmail.com>
2023-11-08 22:34:58 +0530

    Just uploaded Data Structure notes

---
+++ Arrays/Array.py
@@ -0,0 +1,14 @@
+import array
+
+my_array = array.array('i')
+
+print(my_array)
+
+my_array1 = array.array('i', [1, 2, 3])
+print(my_array1)
+
+import numpy as np
+np_array = np.array([], dtype=int)
+print(np_array)
+np_array1 = np.array([1, 2, 3])
+print(np_array1)
--- Arrays/array.py
+++
@@ -1,7 +0,0 @@
-import array
-my_array = array.array('i')
-
-print(my_array)
-
-my_array = array.array('1', '2', '3')
-
