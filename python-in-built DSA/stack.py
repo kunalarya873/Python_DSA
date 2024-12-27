3edde587450571b3ff9b86ee910d2cd1dc0e50d6
kunalarya873 <kunalarya873@gmail.com>
2024-12-27 23:51:20 +0530

    stack using deque

--- python-in-built DSA/stack.py
+++ python-in-built DSA/stack.py
@@ -1,3 +1,4 @@
+# Stack implementation using list
 stack = []
 
 stack.append('a')
@@ -15,7 +16,20 @@ stack.append('c')
 
 
 
+# Stack implementation using deque
 from collections import deque
 
-stack = deque()
+stack = deque(maxlen=3)
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print("stack is ",stack)  # ['a', 'b', 'c']
+print("pop from stack is ",stack.pop())  # c
+print("stack is ",stack)  # ['a', 'b']
+print("pop from stack is ",stack.pop())  # b
+print("stack is ",stack)  # ['a']
+print("pop from stack is ",stack.pop())  # a
+print("stack is ",stack)  # []
+print("pop from stack is ",stack.pop())  # IndexError: pop from an empty deque
+
 
