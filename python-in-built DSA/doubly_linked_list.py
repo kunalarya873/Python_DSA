efa3faeec542a5e4afb0491ac8cfeeb792dcdbb8
kunalarya873 <kunalarya873@gmail.com>
2025-01-27 13:52:33 +0530

    Adding Doubly LinkedList

---
+++ python-in-built DSA/doubly_linked_list.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__ (self, data):
+        self.pref = None
+        self.data = data
+        self.nref = None
+    
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def add_starting(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.nref = self.head
+            self.head.pref = new_node
+            self.head = new_node
+    
+    def printdoublylinkedlist(self):
+        if self.head is None:
+            print("Linked List is empty")
+        
+        else:
+            temp = self.head
+            while temp is not None:
+                print(temp.data, " --> ", end="")
+                temp = temp.nref
+    
+    def printdoublylinkedlistreverse(self):
+        if self.head is None:
+            print("Linked List is empty")
+        
+        else:
+            temp = self.head
+            while temp is not None:
+                ptr = temp
+                temp = temp.nref
+            while ptr is not None:
+                print(ptr.data, "-->", end="")
+                ptr= ptr.pref
+            
+
+
+obj1 = DoublyLinkedList()
+obj1.printdoublylinkedlist()
+obj1.add_starting(20)
+obj1.add_starting(2000)
+obj1.add_starting(210)
+obj1.add_starting(1120)
+obj1.printdoublylinkedlist()
+print()
+obj1.printdoublylinkedlistreverse()
+print()
