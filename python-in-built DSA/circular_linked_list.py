e20d8acfe7f3d1bfa2dcf7c0e64013a48df3af0b
kunalarya873 <kunalarya873@gmail.com>
2025-03-04 23:53:21 +0530

    Adding print method in CLL

--- python-in-built DSA/circular_linked_list.py
+++ python-in-built DSA/circular_linked_list.py
@@ -11,9 +11,30 @@ class CircularLinkedList:
         new_node = Node(data)
         if self.head is None:
             new_node.ref = new_node
-        new_node.ref = self.head
-        temp = self.head
-        while(temp.ref != self.head):
-            temp = temp.ref
-        temp.ref = new_node
-        self.head = new_node
+            self.head = new_node
+        else:
+            new_node.ref = self.head
+            temp = self.head
+            while(temp.ref != self.head):
+                temp = temp.ref
+            temp.ref = new_node
+            self.head = new_node
+    
+    def print_ll(self):
+        if self.head is None:
+            print("LL is empty")
+        else:
+            n = self.head
+            while True:
+                print(n.data, "->", end=" ")
+                n = n.ref
+                if n == self.head:  # Stop when we reach head again
+                    break
+            print()
+
+obj1 = CircularLinkedList()
+obj1.add_starting(10)
+obj1.add_starting(20)
+obj1.add_starting(30)
+obj1.add_starting(40)
+obj1.print_ll()
