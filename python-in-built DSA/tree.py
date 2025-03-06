4b645e453b9580acd823ebec79f4fe1f20fdc984
kunalarya873 <kunalarya873@gmail.com>
2025-03-06 16:44:16 +0530

    Starting Tree

---
+++ python-in-built DSA/tree.py
@@ -0,0 +1,43 @@
+class BinaryST:
+    def __init__(self, data):
+        self.data = data
+        self.lchild = None
+        self.rchild = None  # Fixed naming
+
+    def insertvalue(self, value):
+        if value < self.data:  # Use "<" for left child
+            if self.lchild:
+                self.lchild.insertvalue(value)
+            else:
+                self.lchild = BinaryST(value)
+        else:  # Use ">=" for right child
+            if self.rchild:
+                self.rchild.insertvalue(value)
+            else:
+                self.rchild = BinaryST(value)
+
+    def inorder_traversal(self):  # Added traversal for testing
+        if self.lchild:
+            self.lchild.inorder_traversal()
+        print(self.data, end=" ")
+        if self.rchild:
+            self.rchild.inorder_traversal()
+
+# Create the root node
+root = BinaryST(10)
+print("Root:", root.data)
+
+# Insert values
+root.insertvalue(39)
+root.insertvalue(5)
+root.insertvalue(20)
+root.insertvalue(1)
+
+# Print children properly
+print("Left child of root:", root.lchild.data if root.lchild else None)
+print("Right child of root:", root.rchild.data if root.rchild else None)
+
+# Inorder traversal to verify correctness
+print("Inorder Traversal of BST:")
+root.inorder_traversal()
+print()
