35287a868b0a78cae7e829e5e1d40d924c179f98
kunalarya873 <kunalarya873@gmail.com>
2025-03-09 00:21:41 +0530

    Adding avl print method

--- python-in-built DSA/avl_tree.py
+++ python-in-built DSA/avl_tree.py
@@ -6,6 +6,23 @@ class Node:
         self.height = 1
 
 class AVL_tree:
-    def __init__(self):
-        self.head = None
+    def insert(self, root, key):
+        if not root:
+            return Node(key)
+        elif key < root.data:
+            root.lchild = self.insert(root.lchild.key)
+        else:
+            root.rchild = self.insert(root.rchild.key)
+        root.height = 1 + max(self.getHeight(root.lchild), self.getHeight(root.rchild))
+        balance = self.getBalance(root)
+        return root
+
+    def getHeight(self, root):
+        if not root:
+            return 0
+        return root.height
     
+    def getBalance(self, root):
+        if not root:
+            return 0
+        return self.getHeight(root.lchild) - self.getHeight(root.rchild)
