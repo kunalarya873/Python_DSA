302fb47846b9c082024be49f71db22fb8724cb0f
kunalarya873 <kunalarya873@gmail.com>
2025-03-16 20:08:11 +0530

    Adding DFS in graphs

--- python-in-built DSA/graphs.py
+++ python-in-built DSA/graphs.py
@@ -1,3 +1,4 @@
+# BFS
 graph = {
     '5': ['4', '3', '2'],
     '4': ['5', '2', '3'],
@@ -22,6 +23,31 @@ def bfs(visited, graph, node):
                 visited.append(neighbour)
                 queue.append(neighbour)
 
-print("output of bfs")
-bfs(visited, graph, '5')
+# print("output of bfs")
+# bfs(visited, graph, '5')
+# print()
+
+
+# DFS
+
+graph = {
+    '5': ['3', '7'],
+    '3': ['2', '4'],
+    '7': ['8'],
+    '2': ['9'],
+    '4': ['9'],
+    '8': ['9'],
+    '9': []
+}
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+print('dfs output')
+dfs(visited, graph, '5')
 print()
