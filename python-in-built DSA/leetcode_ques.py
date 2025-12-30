45061d42d74558c843b8e50bd83da331794739eb
kunalarya873 <kunalarya873@gmail.com>
2025-12-30 13:45:27 +0530

    Refactor armstrongNumber function to use a while loop for digit extraction and calculation

--- python-in-built DSA/leetcode_ques.py
+++ python-in-built DSA/leetcode_ques.py
@@ -4,11 +4,36 @@
 def armstrongNumber(num: int)-> bool:
     total = 0
     nod = list(str(num))
-    for i in nod:
-        total += int(i)**len(nod)
+
+    while num> 0:
+        last_digit = num%10
+        total += last_digit*len(nod)
+        num=num//10
     return num == total
 
-print(armstrongNumber(153))
-print(armstrongNumber(128))
+# print(armstrongNumber(153))
+# print(armstrongNumber(128))
 
+
+import math
+# Print all factors of a given number
+def giveFactors(num: int):
+    factors = set()
+    factors.add(1)
+    i = 2
+    while i<math.sqrt(num)+1:
+        if num%i == 0:
+            factors.add(i)
+            factors.add(num//i)
+        i+=1
+    factors.add(num)
+    return factors
+
+# print(giveFactors(10))
+# print(giveFactors(36))
+# print(giveFactors(8888))
+
+
+
+# Frequency in a dictionary
 
