c1ad4f953eac71d4b276b1e660c149164ed0eb5f
kunalarya873 <kunalarya873@gmail.com>
2026-01-01 18:21:26 +0530

    Add insertionSort function for sorting arrays; update bubbleSort example

--- python-in-built DSA/leetcode_ques.py
+++ python-in-built DSA/leetcode_ques.py
@@ -131,3 +131,15 @@ def bubbleSort(arr: list)-> list:
     return arr
 
 # print(bubbleSort([3, 5,63, 6, 2, 6, 11, 6, 1, 3,5,6,]))
+
+def insertionSort(arr:list)->list:
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >=0 and arr[j]<key:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
+    return arr
+
+print(insertionSort([3, 5,63, 6, 2, 6, 11, 6, 1, 3,5,6,]))
