4dae731bfc25869de93fa1e0daa83d0bea2a5cf5
kunalarya873 <kunalarya873@gmail.com>
2026-01-01 14:01:07 +0530

    Add bubbleSort function for sorting arrays; update selectionSortIncr example

--- python-in-built DSA/leetcode_ques.py
+++ python-in-built DSA/leetcode_ques.py
@@ -120,4 +120,14 @@ def selectionSortIncr(arr: list)-> list:
     return arr
 
 
-print(selectionSortIncr([3, 5,63, 6, 2, 6, 11, 6, 1, 3,5,6,]))
+# print(selectionSortIncr([3, 5,63, 6, 2, 6, 11, 6, 1, 3,5,6,]))
+
+def bubbleSort(arr: list)-> list:
+    for i in range(len(arr)-2, -1, -1):
+        for j in range(0, i+1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+# print(bubbleSort([3, 5,63, 6, 2, 6, 11, 6, 1, 3,5,6,]))
