6a6fe7c4299ec93d13270a78125ee43a9b9c8b44
kunalarya873 <kunalarya873@gmail.com>
2026-01-03 15:16:24 +0530

    Add functions to rotate a matrix by 90 degrees; update test case

--- python-in-built DSA/leetcode_ques.py
+++ python-in-built DSA/leetcode_ques.py
@@ -485,3 +485,29 @@ def isPalindrome(s: str) -> bool:
             return True
         else:
             return False
+
+
+def rotateArrayby90deg(arr:list)-> list:
+    n = len(arr)
+    new_arr = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            new_arr[j][n-1-i] = arr[i][j]
+    arr = new_arr
+    return new_arr
+
+def rotateArray(arr:list)-> None:
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            arr[i][j], arr[j][i] = arr[j][i], arr[i][j]
+
+    for i in arr:
+        i.reverse()
+    return
+
+
+arr = [[1,2,3],[4,5,6],[7,8,9]]
+rotateArray(arr)
+
+print(arr)
